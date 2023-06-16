651a72f08ddb39dfce496a44e7a0d115f87596dd
Rafael Noll <85701101+Rafaelnoll@users.noreply.github.com>
2023-06-15 23:00:27 -0300

    Adicionando página com métodos de segurança na web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,42 +5,23 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
-import { SignupComponent } from './pages/signup/signup.component';
-import { ReportsComponent } from './pages/reports/reports.component';
-import { MasterComponent } from './pages/master/master.component';
-import { EsqueceuSenhaComponent } from './pages/esqueceu-senha/esqueceu-senha.component';
-import { AuthGuards } from './pages/guards/auth-guards';
-import { AuthService } from './pages/login/auth.service';
-import { Pagina2Component } from './pages/home/pagina2/pagina2.component';
+import { MetodosComponent } from './pages/metodos/metodos.component';
 import { HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     HomeComponent,
-    SignupComponent,
-    ReportsComponent,
-    MasterComponent,
-    EsqueceuSenhaComponent,
-    Pagina2Component,
-
-
-
-
+    MetodosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-
-
-
   ],
-  providers: [AuthGuards,AuthService,HttpClient],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
